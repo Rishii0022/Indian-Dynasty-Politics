1b6f46f0f45a7bf63be95d7a4bed023293edc744
Rishi0022 <rishi.im22@gmail.com>
2026-01-09 03:07:34 -0800

    final before retrive 001

--- api key/StructuredOutputRelatives.py
+++ api key/StructuredOutputRelatives.py
@@ -4,9 +4,9 @@ from typing import Dict, List, Union
 #extra="forbid"
 
 class Relatives(BaseModel):
-    Relation:str = Field(description = "Relation with the relative")
+    Relation:str = Field(description = "Relation with the winning elected candidate")
     Name:str = Field(description = "Name of the relative")
-    PoliticalRole:str = Field(description="Political role/position")
+    PoliticalRole:str = Field(description="Political role/position of the relative")
     YearsHeld:  Union[str, int] = Field(description="Years held")
     ConstituencyName: str = Field(description="Constituency name")
     DistrictName: str = Field(description="District name")
--- api key/StructuredOutputURL.py
+++ api key/StructuredOutputURL.py
@@ -1,3 +1,5 @@
+from dataclasses import Field
+
 from pydantic import BaseModel
 from typing import List
 
