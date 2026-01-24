9c4b0fbf257dd08087f87e1c77017784da8099bd
Rishi0022 <rishi.im22@gmail.com>
2026-01-24 14:42:51 -0800

    added files for PC parsing

--- Creating Files/createCSV.py
+++ Creating Files/createCSV.py
@@ -1,6 +1,6 @@
 import os
 
-file_path = r"C:\Users\Rishi\Desktop\final dashboard\Final parsed tables\Sources tables(AC recent)\NagalandAC_sources_recent.csv"
+file_path = r"C:\Users\Rishi\Desktop\final dashboard\Final parsed tables\Sources tables(PC ALL)\LokSabha20091_sources_recent.csv"
 
 os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
--- Creating Files/createJSONL.py
+++ Creating Files/createJSONL.py
@@ -1,3 +1,3 @@
 # open(r"absolute_folder_path\file_name_for_jsonl_you_want", "w").close()
 
-open(r"C:\Users\Rishi\Desktop\final dashboard\prompt errors\AC recent errors\WestBengalAC_errors_recent.jsonl", "w").close()
+open(r"C:\Users\Rishi\Desktop\final dashboard\prompt errors\PC all errors\LokSabha2024_errors.jsonl", "w").close()
